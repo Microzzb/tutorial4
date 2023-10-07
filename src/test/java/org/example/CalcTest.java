b4ade81c9bfe70a43214735b6a0c11d8eb0317c9
Zzb <2623763698@qq.com>
2023-10-07 19:31:14 +0800

    Create build.yml
    1

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -8,7 +8,8 @@ public class CalcTest {
 
     @Test
     void testAddition() {
-        assertEquals(4, c.add(2,2));
-    }
+        assertEquals(4, c.add(2,2));}
+    @Test
+    void testSubtraction(){assertEquals(2, c.subtract(4,2));}
 
 }
